629fbbba496fa09a0e78ac131698b67b4934db70
Aaron Hill <aa1ronham@gmail.com>
2016-01-16 12:03:16 -0500

    Add PaginationList. Improve PaginationBuilder

--- src/main/java/org/spongepowered/common/service/pagination/SpongePaginationBuilder.java
+++ src/main/java/org/spongepowered/common/service/pagination/SpongePaginationBuilder.java
@@ -29,12 +29,14 @@ import static org.spongepowered.api.command.CommandMessageFormatting.error;
 
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.Maps;
+import org.spongepowered.api.command.CommandException;
+import org.spongepowered.api.command.source.ProxySource;
 import org.spongepowered.api.service.pagination.PaginationBuilder;
 import org.spongepowered.api.service.pagination.PaginationCalculator;
+import org.spongepowered.api.service.pagination.PaginationList;
 import org.spongepowered.api.text.Text;
-import org.spongepowered.api.command.CommandException;
-import org.spongepowered.api.command.CommandSource;
-import org.spongepowered.api.command.source.ProxySource;
+import org.spongepowered.api.text.channel.MessageChannel;
+import org.spongepowered.api.text.channel.MessageReceiver;
 
 import java.util.List;
 import java.util.Map;
@@ -42,12 +44,15 @@ import java.util.stream.Collectors;
 import java.util.stream.StreamSupport;
 
 class SpongePaginationBuilder implements PaginationBuilder {
+
     private final SpongePaginationService service;
     private Iterable<Text> contents;
     private Text title;
     private Text header;
     private Text footer;
     private String paginationSpacer = "=";
+
+    private PaginationList paginationList;
 
     public SpongePaginationBuilder(SpongePaginationService service) {
         this.service = service;
@@ -56,79 +61,87 @@ class SpongePaginationBuilder implements PaginationBuilder {
     @Override
     public PaginationBuilder contents(Iterable<Text> contents) {
         this.contents = contents;
+        this.paginationList = null;
         return this;
     }
 
     @Override
     public PaginationBuilder contents(Text... contents) {
         this.contents = ImmutableList.copyOf(contents);
+        this.paginationList = null;
         return this;
     }
 
     @Override
     public PaginationBuilder title(Text title) {
         this.title = title;
+        this.paginationList = null;
         return this;
     }
 
     @Override
     public PaginationBuilder header(Text header) {
         this.header = header;
+        this.paginationList = null;
         return this;
     }
 
     @Override
     public PaginationBuilder footer(Text footer) {
         this.footer = footer;
+        this.paginationList = null;
         return this;
     }
 
     @Override
     public PaginationBuilder paddingString(String padding) {
         this.paginationSpacer = padding;
+        this.paginationList = null;
         return this;
     }
 
-    @SuppressWarnings("rawtypes")
     @Override
-    public void sendTo(final CommandSource source) {
+    public PaginationList build() {
         checkNotNull(this.contents, "contents");
-        checkNotNull(source, "source");
-        this.service.registerCommandOnce();
 
-        CommandSource realSource = source;
-        while (realSource instanceof ProxySource) {
-            realSource = ((ProxySource)realSource).getOriginalSource();
+        if (this.paginationList == null) {
+            this.paginationList = new SpongePaginationList(this.service, this.contents, this.title, this.header, this.footer);
         }
-        @SuppressWarnings("unchecked")
-        PaginationCalculator<CommandSource> calculator = (PaginationCalculator) this.service.calculators.get(realSource.getClass());
-        if (calculator == null) {
-            calculator = this.service.getUnpaginatedCalculator(); // TODO: or like 50 lines?
-        }
-        final PaginationCalculator<CommandSource> finalCalculator = calculator;
-        Iterable<Map.Entry<Text, Integer>> counts = StreamSupport.stream(this.contents.spliterator(), false).map(input -> {
-            int lines = finalCalculator.getLines(source, input);
-            return Maps.immutableEntry(input, lines);
-        }).collect(Collectors.toList());
+        return this.paginationList;
+    }
 
-        Text title = this.title;
-        if (title != null) {
-            title = calculator.center(source, title, this.paginationSpacer);
-        }
+    @Override
+    public void sendTo(MessageReceiver source) {
+        this.build().sendTo(source);
+    }
 
-        ActivePagination pagination;
-        if (this.contents instanceof List) { // If it started out as a list, it's probably reasonable to copy it to another list
-            pagination = new ListPagination(source, calculator, ImmutableList.copyOf(counts), title, this.header, this.footer, this.paginationSpacer);
-        } else {
-            pagination = new IterablePagination(source, calculator, counts, title, this.header, this.footer, this.paginationSpacer);
-        }
+    @Override
+    public void sendTo(MessageChannel channel) {
+        this.build().sendTo(channel);
+    }
 
-        this.service.getPaginationState(source, true).put(pagination);
-        try {
-            pagination.nextPage();
-        } catch (CommandException e) {
-            source.sendMessage(error(e.getText()));
-        }
+    @Override
+    public PaginationBuilder from(PaginationList list) {
+        this.reset();
+        this.contents = list.getContents();
+        this.title = list.getTitle();
+        this.header = list.getHeader();
+        this.footer = list.getFooter();
+        this.paginationSpacer = list.getPaddingString();
 
+        this.paginationList = null;
+        return this;
+    }
+
+    @Override
+    public PaginationBuilder reset() {
+        this.contents = null;
+        this.title = null;
+        this.header = null;
+        this.footer = null;
+        this.paginationSpacer = "=";
+
+        this.paginationList = null;
+        return this;
     }
 }
